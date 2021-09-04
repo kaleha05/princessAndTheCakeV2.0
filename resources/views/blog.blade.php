b9e02b7e625c3196a07b2aeb135be84e9c061a8a
kaleha05 <41241095+kaleha05@users.noreply.github.com>
2021-09-04 19:15:10 +0300

    updates blog.blade.php

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,8 +1,7 @@
-@extends('layouts.app')
-@section('content')
+@include('layouts.includes.header')
    
     <!--================End Main Header Area =================-->
-    <section class="banner_area">
+    <!-- <section class="banner_area">
         <div class="container">
             <div class="banner_text">
                 <h3>Blog</h3>
@@ -12,7 +11,7 @@
                 </ul>
             </div>
         </div>
-    </section>
+    </section> -->
     <!--================End Main Header Area =================-->
     
     <!--================Blog Main Area =================-->
@@ -82,4 +81,30 @@
         </section>
         <!--================End Footer Area =================-->
 
-@endsection
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Rev slider js -->
+        <script src="vendors/revolution/js/jquery.themepunch.tools.min.js"></script>
+        <script src="vendors/revolution/js/jquery.themepunch.revolution.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.video.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+        <!-- Extra plugin js -->
+        <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="vendors/magnifc-popup/jquery.magnific-popup.min.js"></script>
+        <script src="vendors/datetime-picker/js/moment.min.js"></script>
+        <script src="vendors/datetime-picker/js/bootstrap-datetimepicker.min.js"></script>
+        <script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+        <script src="vendors/jquery-ui/jquery-ui.min.js"></script>
+        <script src="js/comingsoon.js"></script>
+        <script src="vendors/lightbox/simpleLightbox.min.js"></script>
+        
+        <script src="js/theme.js"></script>
+    </body>
+
+</html>
