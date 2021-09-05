398fc27337aedf45ad7d1df3166a6c8f14ded9b0
kaleha05 <keyagloria@gmail.com>
2021-09-05 14:36:43 +0300

    added flavours to db

--- resources/views/flavours.blade.php
+++ resources/views/flavours.blade.php
@@ -21,7 +21,7 @@
             <div class="cake_feature_row row">
                 @forelse ($entries as $entry)
                     @switch($entry->id)
-                    @case(1)
+                    @case(5)
                     <div class="col-lg-3 col-md-4 col-6">
                         <div class="cake_feature_item">
                             <div class="cake_img">
@@ -30,12 +30,12 @@
                             <div class="cake_text">
                                 <h4><i class="fa fa-heart"></i>{{ $entry->likes}}</h4>
                                 <h3>Raspberry orange with raspberry filling</h3>
-                                <a class="pest_btn" href="./flavours/1" @if($disableButton) disabled='disabled' @endif >Like</a>
+                                <a class="pest_btn" href="./flavours/5" @if($disableButton) disabled='disabled' @endif >Like</a>
                             </div>
                         </div>
                     </div>
                         @break
-                    @case(2)
+                    @case(15)
                     <div class="col-lg-3 col-md-4 col-6">
                         <div class="cake_feature_item">
                             <div class="cake_img">
@@ -44,12 +44,12 @@
                             <div class="cake_text">
                                 <h4><i class="fa fa-heart"></i> {{ $entry->likes}}</h4>
                                 <h3>Lemon blueberry with blueberry filling</h3>
-                                <a class="pest_btn" href="./flavours/2" @if($disableButton) disabled='disabled' @endif >Like</a>
+                                <a class="pest_btn" href="./flavours/15" @if($disableButton) disabled='disabled' @endif >Like</a>
                             </div>
                         </div>
                     </div>
                         @break
-                    @case(3)
+                    @case(35)
                     <div class="col-lg-3 col-md-4 col-6">
                         <div class="cake_feature_item">
                             <div class="cake_img">
@@ -58,12 +58,12 @@
                             <div class="cake_text">
                                 <h4><i class="fa fa-heart"></i> {{ $entry->likes}}</h4>
                                 <h3>Chocolate caramel</h3>
-                                <a class="pest_btn" href="./flavours/3" @if($disableButton) disabled='disabled' @endif >Like</a>
+                                <a class="pest_btn" href="./flavours/35" @if($disableButton) disabled='disabled' @endif >Like</a>
                             </div>
                         </div>
                     </div>
                         @break
-                    @case(4)
+                    @case(45)
                     <div class="col-lg-3 col-md-4 col-6">
                         <div class="cake_feature_item">
                             <div class="cake_img">
@@ -72,12 +72,12 @@
                             <div class="cake_text">
                                 <h4><i class="fa fa-heart"></i> {{ $entry->likes}}</h4>
                                 <h3>Fluffy vanilla with strawberry filling</h3>
-                                <a class="pest_btn" href="./flavours/4" @if($disableButton) disabled='disabled' @endif >Like</a>
+                                <a class="pest_btn" href="./flavours/45" @if($disableButton) disabled='disabled' @endif >Like</a>
                             </div>
                         </div>
                     </div>
                         @break
-                    @case(5)
+                    @case(55)
                     <div class="col-lg-3 col-md-4 col-6">
                         <div class="cake_feature_item">
                             <div class="cake_img">
@@ -86,12 +86,12 @@
                             <div class="cake_text">
                                 <h4><i class="fa fa-heart"></i> {{ $entry->likes}}</h4>
                                 <h3>Pink strawberry and Oreo crunch</h3>
-                                <a class="pest_btn" href="./flavours/5" @if($disableButton) disabled='disabled' @endif >Like</a>
+                                <a class="pest_btn" href="./flavours/55" @if($disableButton) disabled='disabled' @endif >Like</a>
                             </div>
                         </div>
                     </div>
                         @break
-                    @case(6)
+                    @case(65)
                     <div class="col-lg-3 col-md-4 col-6">
                         <div class="cake_feature_item">
                             <div class="cake_img">
@@ -100,7 +100,7 @@
                             <div class="cake_text">
                                 <h4><i class="fa fa-heart"></i> {{ $entry->likes}}</h4>
                                 <h3>Red velvet N' Nutella</h3>
-                                <a class="pest_btn" href="./flavours/6" @if($disableButton) disabled='disabled' @endif >Like</a>
+                                <a class="pest_btn" href="./flavours/65" @if($disableButton) disabled='disabled' @endif >Like</a>
                             </div>
                         </div>
                     </div>
